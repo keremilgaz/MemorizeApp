fcc9f755bc1e7ac603dca8ba25c0f68db5a2fdb5
keremilgaz <keroo_ilgaz@hotmail.com>
2024-06-01 16:12:31 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kerem Ilgaz on 1.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
