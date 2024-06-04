a1ea0db5ba72c44bbb743e05fc6b2ea03e1dc9b9
keremilgaz <keroo_ilgaz@hotmail.com>
2024-06-04 20:01:00 +0300

    Togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
         }
+        .foregroundColor(.black)
         .padding()
     }
 }
+
+struct CardView: View{
+    @State var isFaceUp = false
+    let base = RoundedRectangle(cornerSize: CGSize(width: 12, height: 12))
+    var body: some View{
+        ZStack {
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 1)
+                Text("❤️").font(.largeTitle)
+            }
+            else{
+                base.fill().strokeBorder(.gray)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
 
 #Preview {
     ContentView()
